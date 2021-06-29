1043037f9d681a1187e70d881efc65abe1e35b77
C-Perfume <akdl1020@gmail.com>
2021-06-29 13:59:22 +0900

    a

---
+++ Project_4th/Assets/Star/Scripts/Clones.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clones : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    public void OnClickClone() {
+
+        GameObject a = Instantiate(gameObject);
+        a.transform.position = Vector3.one*.1f;
+        a.transform.localScale = transform.localScale * .1f;
+    }
+}
