7dc2889aeab1eebb19f8389c4dd3f219e7c8ea85
SeungHyunKimm <skyblue051@naver.com>
2021-07-05 16:48:11 +0900

    ..

--- Project_4th/Assets/KSH/03.Scripts/MenuController.cs
+++ Project_4th/Assets/KSH/03.Scripts/MenuController.cs
@@ -8,12 +8,22 @@ public class MenuController : MonoBehaviour
     //메인메뉴를 클릭하게 되면
     //자기자신(MainMenu)는 없어지고 SubMenu 8가지가 나타나게 된다.
 
-    public GameObject SubMenu;
-    public GameObject MainMenu;
-    public GameObject EditMenu;
-    public GameObject FurnitureSlate;
+    GameObject SubMenu;
+    GameObject MainMenu;
+    GameObject EditMenu;
+    GameObject FurnitureSlate;
 
-   
+    private void Start()
+    {
+        SubMenu = GameObject.Find("MainMenu");
+        MainMenu = GameObject.Find("SubMenu");
+        EditMenu = GameObject.Find("EditMenu");
+        FurnitureSlate = GameObject.Find("FurnitureSlate");
+    }
+
+
+
+
     public void OnClickMainMenu()
     {
         //서브메뉴 8가지가 나타난다.
