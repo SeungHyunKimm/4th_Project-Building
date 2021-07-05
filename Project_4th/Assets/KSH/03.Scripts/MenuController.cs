5e74b12f4caaca492fb2470c0ab0b1f9d9d6a16b
SeungHyunKimm <skyblue051@naver.com>
2021-07-05 14:15:55 +0900

    ..

--- Project_4th/Assets/KSH/03.Scripts/MenuController.cs
+++ Project_4th/Assets/KSH/03.Scripts/MenuController.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-    
-
-
-
 public class MenuController : MonoBehaviour
 {
     //메인메뉴를 클릭하게 되면
@@ -14,18 +10,19 @@ public class MenuController : MonoBehaviour
 
     public GameObject SubMenu;
     public GameObject MainMenu;
-
-
-
+    public GameObject EditMenu;
+   
     public void OnClickMainMenu()
     {
-        //자기자신은 없어지고
-        MainMenu.SetActive(false);
         //서브메뉴 8가지가 나타난다.
         SubMenu.SetActive(true);
-
-
-
+        //자기자신은 없어지고
+        MainMenu.SetActive(false);
     }
+
+    public void OnClickEditMenu()
+    {
+        EditMenu.SetActive(true);
+    }   
 
 }
