50ba33a39d6d8be82128ba7729d3d8d9289303b9
JOHAESEONG <81471744+lo-ji@users.noreply.github.com>
2021-07-08 12:01:25 +0900

    .

---
+++ Project_4th/Assets/SM_OvO/Big Scripts.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigScripts : MonoBehaviour
+{
+ 
+    public void Big()
+    {
+        transform.localScale = new Vector3(5, 5, 5);
+    }
+}
