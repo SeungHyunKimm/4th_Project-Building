09953d53c8e4226f90811f602602b908f0361f7b
JOHAESEONG <81471744+lo-ji@users.noreply.github.com>
2021-07-09 18:57:17 +0900

    mooyahou

---
+++ Project_4th/Assets/SM_OvO/Scripts/Flash_Test.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flash_Test : MonoBehaviour
+{
+    int blank = 0;
+    float R;
+    float G;
+    float B;
+
+    private void Start()
+    {
+
+    }
+
+    public void OnclickButtonToLightColor()
+    {
+        blank++;
+    }
+
+    public void Update()
+    {
+
+
+        if (blank == 1)
+        {
+            Light lg = GetComponent<Light>();
+            lg.color = new Color(50, 50, 50);
+        }
+
+        if(blank == 2)
+        {
+            Light lg = GetComponent<Light>();
+            lg.color = new Color(62, 199, 41);
+
+            if (blank > 3)
+            {
+                blank = 0;
+            }
+
+        }
+
+
+    }
+}
